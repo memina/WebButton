22c2b440be2e11f514f82638a1e1f40250cad91b
memina <eminarvas@gmail.com>
2016-11-03 12:05:11 +0300

    yeni

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
 var sock;
+var ledcount = 2;
 
 app.listen(8001, function(){
  console.log('listening on *.8001');
@@ -9,13 +10,15 @@ app.listen(8001, function(){
 
 // button is attaced to pin 18, led to 17
 var GPIO = require('onoff').Gpio;
-var led1 = new GPIO(2, 'out');
-var led2 = new GPIO(3, 'out');
-//test
-
+var leds = [];
+for (var i = 0; i < ledcount; i++) {
+  leds.push(new GPIO(i+2, 'out'));
+}
 //Init LED to off
-led1.writeSync(0);
-led2.writeSync(0);
+for (var i = 0; i < ledcount; i++) {
+  leds[i].writeSync(0);
+}
+
 
 function handler (req, res) {
   fs.readFile('/root/Node-Button/index.html',
@@ -37,22 +40,11 @@ io.on('connection', function (socket) {
   
   socket.on('led', function (data) {
     console.log(data);
-    led1.writeSync(0);
-    led2.writeSync(0);
-    switch (data) {
-        case "1":
-          led1.writeSync(1);
-          socket.emit('ledstatus', '1');
-            break;
-        case "2":
-          led2.writeSync(1);
-          socket.emit('ledstatus', '2');
-            break;
-           }
-      
-      
-      
-     
+    for (var i = 0; i < ledcount; i++) {
+      leds[i].writeSync(0);
+    }
+    leds[data-1].writeSync(1);
+    socket.emit('ledstatus', data);
     
   });
     
